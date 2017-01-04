49232461e62a3ecb20e0b83bc6061ffbb4475411
Will Duffy <willmanduffy@gmail.com>
2017-01-03 22:24:17 -0500

    Partially through #9

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+import React from 'react';
+
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+
+import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+import './css/style.css';
+
+const Root  = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'));
